ddbc1b30ce81e64694d8733fc3b8f391be0eff7f
Luca Barbosa Bento <lbento@c2r1p1.42sp.org.br>
2025-09-03 18:18:12 -0300

    My first window and pixels

--- src/so_long.h
+++ src/so_long.h
@@ -1,12 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.c                                          :+:      :+:    :+:   */
+/*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lbento <lbento@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/02 15:42:58 by lbento            #+#    #+#             */
-/*   Updated: 2025/09/03 17:23:09 by lbento           ###   ########.fr       */
+/*   Created: 2025/09/03 17:01:28 by lbento            #+#    #+#             */
+/*   Updated: 2025/09/03 17:51:47 by lbento           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "mlx.h"
+# include "libft.h"
+# include <stdio.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <errno.h>
+
+#endif
 
