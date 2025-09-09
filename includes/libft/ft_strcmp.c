f442c28fef101f43b4e54477c171a293e42e7287
Luca Barbosa <lucabento.luca@gmail.com>
2025-09-08 22:01:11 -0300

    ft_strcmp

---
+++ includes/libft/ft_strcmp.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbento <lbento@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/15 21:20:11 by gamarcha          #+#    #+#             */
+/*   Updated: 2025/09/08 22:00:39 by lbento           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strcmp(const char *s1, const char *s2);
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t			i;
+
+	i = 0;
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
+}
