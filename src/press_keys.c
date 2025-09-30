a5b99616e89bab936993d14627539f99530cf5b5
Luca Barbosa Bento <lbento@c3r6p1.42sp.org.br>
2025-09-30 20:27:59 -0300

    Implementing arrows in the game

--- src/press_keys.c
+++ src/press_keys.c
@@ -6,53 +6,53 @@
 /*   By: lbento <lbento@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 11:53:33 by lbento            #+#    #+#             */
-/*   Updated: 2025/09/30 20:09:58 by lbento           ###   ########.fr       */
+/*   Updated: 2025/09/30 20:26:11 by lbento           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	key_pressed(int pressed_key, t_game_manager *game);
-int	key_released(int pressed_key, t_game_manager *game);
-int	destroy_hook(int pressed_key, t_game_manager *game);
+int	key_pressed(int keycode, t_game_manager *game);
+int	key_released(int keycode, t_game_manager *game);
+int	destroy_hook(int keycode, t_game_manager *game);
 
-int	key_pressed(int pressed_key, t_game_manager *game)
+int	key_pressed(int keycode, t_game_manager *game)
 {
-	if (pressed_key == 65307)
+	if (keycode == 65307)
 	{
 		free_game(game);
 		exit(0);
 	}
-	if (pressed_key == 119)
+	if (keycode == 119 || keycode == 65362)
 		game->game->player_up = 1;
-	if (pressed_key == 115)
+	if (keycode == 115 || keycode == 65364)
 		game->game->player_down = 1;
-	if (pressed_key == 97)
+	if (keycode == 97 || keycode == 65361)
 		game->game->player_left = 1;
-	if (pressed_key == 100)
+	if (keycode == 100 || keycode == 65363)
 		game->game->player_right = 1;
 	update(game);
 	return (0);
 }
 
-int	key_released(int pressed_key, t_game_manager *game)
+int	key_released(int keycode, t_game_manager *game)
 {
-	if (pressed_key == 119)
+	if (keycode == 119 || keycode == 65362)
 		game->game->player_up = 0;
-	if (pressed_key == 115)
+	if (keycode == 115 || keycode == 65364)
 		game->game->player_down = 0;
-	if (pressed_key == 97)
+	if (keycode == 97 || keycode == 65361)
 		game->game->player_left = 0;
-	if (pressed_key == 100)
+	if (keycode == 100 || keycode == 65363)
 		game->game->player_right = 0;
 	update(game);
 	return (0);
 }
 
-int	destroy_hook(int pressed_key, t_game_manager *game)
+int	destroy_hook(int keycode, t_game_manager *game)
 {
 	free_game(game);
 	exit(0);
-	(void)pressed_key;
+	(void)keycode;
 	(void)game;
 }
