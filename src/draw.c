a8544081dd4c1e940dd3c70af9d77599bcfb2067
Luca Barbosa <lucabento.luca@gmail.com>
2025-09-30 01:17:56 -0300

    New assets from F1

--- src/draw.c
+++ src/draw.c
@@ -6,7 +6,7 @@
 /*   By: lbento <lbento@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 19:41:07 by lbento            #+#    #+#             */
-/*   Updated: 2025/09/24 16:12:38 by lbento           ###   ########.fr       */
+/*   Updated: 2025/09/30 00:43:51 by lbento           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,9 @@ void	draw(t_game_manager *game)
 		while (i < game->game->width)
 		{
 			if (game->game->map[j][i] == 1)
-				square_map(game, game->wall, i * 40, j * 40);
+				square_map(game, game->wall, i * 32, j * 32);
 			else
-				square_map(game, game->ground, i * 40, j * 40);
+				square_map(game, game->ground, i * 32, j * 32);
 			draw_env(game, i, j);
 			i++;
 		}
@@ -46,10 +46,10 @@ static void	square_map(t_game_manager *game, t_img *img, int x, int y)
 	int				j;
 
 	j = 0;
-	while (j < 40)
+	while (j < 32)
 	{
 		i = 0;
-		while (i < 40)
+		while (i < 32)
 		{
 			color = mlx_get_pixel(img, i, j);
 			if (color != mlx_rgb_to_int(0, 255, 255, 255))
@@ -65,14 +65,14 @@ static void	draw_env(t_game_manager *game, int i, int j)
 	int	k;
 
 	if (game->game->exit.x == i && game->game->exit.y == j)
-		square_map(game, game->exit, i * 40, j * 40);
+		square_map(game, game->exit, i * 32, j * 32);
 	k = 0;
 	while (k < game->game->count_coll)
 	{
 		if (game->game->coll[k].x == i && game->game->coll[k].y == j)
-			square_map(game, game->coll, i * 40, j * 40);
+			square_map(game, game->coll, i * 32, j * 32);
 	k++;
 	}
 	if (game->game->player.x == i && game->game->player.y == j)
-		square_map(game, game->player, i * 40, j * 40);
+		square_map(game, game->player, i * 32, j * 32);
 }
